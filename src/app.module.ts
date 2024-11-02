8cbd7c847b5a0aa7af33ecb7b5d843765d547d62
Bart Karalus <bart@bforge.co>
2024-11-02 18:50:00 +0100

    feat: added content module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContentModule } from './content/content.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), ContentModule],
   controllers: [AppController],
   providers: [AppService],
 })
