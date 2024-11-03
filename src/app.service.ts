cba029ef10d52a5b4b2819e8794a1f09604f6ec5
Bart Karalus <bart@bforge.co>
2024-11-03 12:33:58 +0100

    feat: added health check

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly dataSource: DataSource) {}
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getHealth(): Promise<string> {
+    await this.dataSource.query('SELECT 1');
+    return 'OK';
   }
 }
